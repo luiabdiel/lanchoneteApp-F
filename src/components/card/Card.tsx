f51276cc61ae9ed36296c52bbb54ca9199da7661
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-06-19 10:16:45 -0300

    feat: Formatting prices

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,12 +1,13 @@
 import { SnackData } from '../../interface/SnackData';
-import './Card.module.scss'
+import { priceFormatter } from '../../utils/formatter';
+import Styles from './Card.module.scss'
 
 export function Card({ title, price, image} : SnackData){
     return(
-        <div className='card'>
+        <div className={Styles.card}>
           <img src={image} alt={title} />
           <h2>{title}</h2>
-          <p><b>Valor: {price}</b></p>
+          <p><b>{priceFormatter.format(price)}</b></p>
         </div>
     );
 }
